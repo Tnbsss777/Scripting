6369ef0d74b8cac006b967c5a6a7b64fa8d8909c
Tnbsss777 <tejasbhai7777@gmail.com>
2026-09-15 18:21:57 +0530

    This is my first commit

--- Script1.sh
+++ Script1.sh
@@ -1,4 +1,5 @@
 echo "We are trying this practical from linux EC2"
+<<<<<<< HEAD
 echo "I am from Young minds"
 echo "We are from batch-26 and learning DevOps"
 echo "Hello, Good morning!"
@@ -6,3 +7,9 @@ echo "Hello bhava, kasa ahes? Majet na."
 echo "Ekdum majet"
 echo "Broooooooooo"
 echo "Uno khelayla chala"
+=======
+echo "We are from batch-33 and learning git/gihub"
+echo "We are learning Branching"
+echo "I am learning Git"
+echo "Today is 15/09/26 and we are learning the git"
+>>>>>>> f120dc1 (This is my first commit)
